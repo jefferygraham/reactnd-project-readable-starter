7b1116adc34041283bcc849d0d853f67f3aa176e
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-11-13 15:04:50 -0500

    add views

---
+++ frontend/src/components/Category.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Category extends Component {
+  render() {
+    return <h1>CATEGORY VIEW</h1>;
+  }
+}
+
+export default Category;
---
+++ frontend/src/components/Post.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+  render() {
+    return <h1>POST VIEW</h1>;
+  }
+}
+
+export default Post;
